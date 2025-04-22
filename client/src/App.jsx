13c497fa7d4832e52a5fad7587a0ffcc85da544f
urmishr <ramani.urmish@gmail.com>
2025-04-22 14:44:24 +0530

    Page routing is completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,33 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AllTours from './page/AllTours';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import AppLayout from './page/AppLayout';
+import Settings from './components/Settings';
+import Account from './page/Account';
+import MyBookings from './components/MyBookings';
+import AuthLayout from './page/AuthLayout';
+import TourOverview from './page/TourOverview';
+
 function App() {
-    return <h1>Hello from the natours client</h1>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<AppLayout />}>
+                    <Route index element={<AllTours />} />
+                    <Route path='tour/:id' element={<TourOverview />} />
+                    <Route element={<AuthLayout />}>
+                        <Route path='login' element={<Login />} />
+                        <Route path='signup' element={<Signup />} />
+                    </Route>
+                    <Route path='account' element={<Account />}>
+                        <Route index element={<Settings />} />
+                        <Route path='my-bookings' element={<MyBookings />} />
+                    </Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
